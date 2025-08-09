bcbc8ceeaccdf160125d2a623ca976f5670f3397
cyr-ius <cyr-ius@ipocus.net>
2025-08-09 17:12:28 +0200

    Fix config flow

--- custom_components/reolink_thumbs/config_flow.py
+++ custom_components/reolink_thumbs/config_flow.py
@@ -2,16 +2,9 @@
 
 from __future__ import annotations
 
-import logging
-
-import voluptuous as vol
 from homeassistant import config_entries
 
 from .const import DOMAIN
-
-_LOGGER = logging.getLogger(__name__)
-
-DATA_SCHEMA = vol.Schema({vol.Required("valid", default=True): bool})
 
 
 class HeatzyFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
@@ -21,7 +14,4 @@ class HeatzyFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         """Handle a flow initialized by the user."""
-        if user_input:
-            return self.async_create_entry(title=DOMAIN, data=user_input)
-
-        return self.async_show_form(step_id="user", data_schema=DATA_SCHEMA)
+        return self.async_create_entry(title=DOMAIN, data={})
